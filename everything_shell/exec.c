a7296b6940b18d7293b73bf2abcbd61736a25fd5
Joseph Ita <odohofreh.joseph.i@gmail.com>
2023-09-07 22:17:38 +0000

    Combine fork, wait and execve

--- everything_shell/exec.c
+++ everything_shell/exec.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char *argv[] = {"/bin/ls", "-l", "/usr/", NULL};
+	char *pathname = argv[0];
 	char *envp[] = {NULL};
 
 	printf("Before execve\n");
-	if (execve(argv[0], argv, envp) == -1)
+	if (execve(pathname, argv, envp) == -1)
 	{
 		perror("Error:");
 	}
